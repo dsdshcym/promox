57b53c8cbfff67be4098e39c27a7abb4f898a0fe
Yiming Chen <dsdshcym@gmail.com>
2021-06-01 21:14:17 +0800

    add Promox.State.stub/4 function

--- lib/promox/state.ex
+++ lib/promox/state.ex
@@ -1,6 +1,12 @@
 defmodule Promox.State do
   def new() do
-    %{expects: %{}}
+    %{expects: %{}, stubs: %{}}
+  end
+
+  def stub(state, protocol, callback, fun) do
+    {:arity, arity} = Function.info(fun, :arity)
+
+    put_in(state, [:stubs, {protocol, callback, arity}], fun)
   end
 
   def expect(state, protocol, callback, n \\ 1, fun) do
@@ -19,10 +25,23 @@ defmodule Promox.State do
   end
 
   def retrieve(state, pfa) do
+    {expect, new_state} = pop_expect(state, pfa)
+
+    {
+      expect || get_stub(state, pfa),
+      new_state
+    }
+  end
+
+  defp pop_expect(state, pfa) do
     get_and_update_in(state, [:expects, pfa], fn
       nil -> {nil, nil}
       [] -> {nil, []}
       [expect | rest] -> {expect, rest}
     end)
+  end
+
+  defp get_stub(state, pfa) do
+    get_in(state, [:stubs, pfa])
   end
 end
--- test/promox/state_test.exs
+++ test/promox/state_test.exs
@@ -11,6 +11,48 @@ defmodule Promox.StateTest do
     end
   end
 
+  describe "stub/4" do
+    test "stubs 1 function that can be retrieved later" do
+      state = State.new() |> State.stub(Calculable, :add, fn 1, 2 -> :stubbed_add end)
+
+      {fun, _} = State.retrieve(state, {Calculable, :add, 2})
+      assert fun.(1, 2) == :stubbed_add
+    end
+
+    test "stubs 1 function that can be retrieved multiple times later" do
+      state = State.new() |> State.stub(Calculable, :add, fn 1, 2 -> :stubbed_add end)
+
+      {fun, state1} = State.retrieve(state, {Calculable, :add, 2})
+      assert fun.(1, 2) == :stubbed_add
+
+      {fun, _state2} = State.retrieve(state1, {Calculable, :add, 2})
+      assert fun.(1, 2) == :stubbed_add
+    end
+
+    test "overwrites previous stub/4 call" do
+      state =
+        State.new()
+        |> State.stub(Calculable, :add, fn 1, 2 -> :stubbed_add end)
+        |> State.stub(Calculable, :add, fn 1, 2 -> :override_stubbed_add end)
+
+      {fun, _} = State.retrieve(state, {Calculable, :add, 2})
+      assert fun.(1, 2) == :override_stubbed_add
+    end
+
+    test "stubs multiple functions that can be retrieved later" do
+      state =
+        State.new()
+        |> State.stub(Calculable, :add, fn 1, 2 -> :stubbed_add end)
+        |> State.stub(Calculable, :mult, fn 3, 4 -> :stubbed_mult end)
+
+      {fun, _} = State.retrieve(state, {Calculable, :add, 2})
+      assert fun.(1, 2) == :stubbed_add
+
+      {fun, _} = State.retrieve(state, {Calculable, :mult, 2})
+      assert fun.(3, 4) == :stubbed_mult
+    end
+  end
+
   describe "expect/5" do
     test "stubs 1 function that can be retrieved later" do
       state = State.new() |> State.expect(Calculable, :add, fn 1, 2 -> :stubbed_add end)
@@ -43,6 +85,33 @@ defmodule Promox.StateTest do
 
       {mult, _after_mult} = State.retrieve(after_add, {Calculable, :mult, 2})
       assert mult.(3, 4) == :stubbed_mult
+    end
+  end
+
+  describe "expect/5 and stub/4" do
+    test "retrieves expect over stub" do
+      state =
+        State.new()
+        |> State.stub(Calculable, :add, fn 1, 2 -> :stubbed_add end)
+        |> State.expect(Calculable, :add, fn 1, 2 -> :expect_add end)
+        |> State.stub(Calculable, :add, fn 1, 2 -> :stubbed_add end)
+
+      {fun, _} = State.retrieve(state, {Calculable, :add, 2})
+      assert fun.(1, 2) == :expect_add
+    end
+
+    test "falls back to stub when expects are used-up" do
+      state =
+        State.new()
+        |> State.stub(Calculable, :add, fn 1, 2 -> :stubbed_add end)
+        |> State.expect(Calculable, :add, fn 1, 2 -> :expect_add end)
+        |> State.stub(Calculable, :add, fn 1, 2 -> :override_stubbed_add end)
+
+      {fun, state1} = State.retrieve(state, {Calculable, :add, 2})
+      assert fun.(1, 2) == :expect_add
+
+      {fun, _state2} = State.retrieve(state1, {Calculable, :add, 2})
+      assert fun.(1, 2) == :override_stubbed_add
     end
   end
 end
