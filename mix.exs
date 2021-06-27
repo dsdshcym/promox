8758718f0aec3b23801bcf4e14e0966f62d835e2
Yiming Chen <dsdshcym@gmail.com>
2021-06-27 21:10:54 +0800

    bump version to 0.1.1
    - [1: 7866fbb]
    
    1: 7866fbbc7702795735182aa8ab7972864416de12
       Allow defmock to accept a variable as the protocol module

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Promox.MixProject do
   def project do
     [
       app: :promox,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
