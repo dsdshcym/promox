608b643e9f5b86423b43372122628b014937a360
Yiming Chen <dsdshcym@gmail.com>
2021-06-28 15:06:24 +0800

    bump minimum support elixir version to 1.5
    - Macro.generate_arguments/2 was introduced in 1.5.0
      see https://github.com/elixir-lang/elixir/blob/v1.12.1/lib/elixir/lib/macro.ex#L354-L357

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Promox.MixProject do
     [
       app: :promox,
       version: "0.1.1",
-      elixir: "~> 1.0",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       name: "Promox",
