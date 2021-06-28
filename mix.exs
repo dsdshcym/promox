2c20d0962b7a966e21166358e3c65ef8e85de408
Yiming Chen <dsdshcym@gmail.com>
2021-06-28 15:11:57 +0800

    bump version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Promox.MixProject do
   def project do
     [
       app: :promox,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
